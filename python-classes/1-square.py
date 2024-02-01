ece445a3ae33431646293f9b5d1c0e98c6b3d35a
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 <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>
2024-01-31 16:18:00 -0800

    a class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """a class Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size):
+        """Constructor method"""
+        self.__size = size
