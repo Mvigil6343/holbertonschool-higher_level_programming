d240c10f1df48c845feece961670d2291a635b57
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 <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>
2024-01-31 23:05:37 -0800

    a class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,6 +13,10 @@ class Square:
     def size(self):
         return self.__size
 
+    @property
+    def position(self):
+        return self.__position
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -22,17 +26,15 @@ class Square:
         else:
             self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0\
-                or type(value[0]) is not int or type(value[1]) is not int:
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if not all(isinstance(idx, int) for idx in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(idx >= 0 for idx in value):
             raise TypeError('position must be a tuple of 2 positive integers')
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """method that returns the area of the square"""
