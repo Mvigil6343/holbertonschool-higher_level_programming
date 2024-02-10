b5efbf37d736df98c0891cf28cab901d57fdbff1
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 <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>
2024-02-10 12:38:10 -0800

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry."""
+
+    def area(self):
+        """area of BaseGeometry"""
+        raise Exception("area() is not implemented")
