9e183acedac387cf55c76e07191db02fc2b9c35d
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 <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>
2024-02-14 16:20:30 -0800

    3. Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,8 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -36,7 +37,8 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -48,7 +50,8 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -60,4 +63,5 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        else:
+            self.__y = value
