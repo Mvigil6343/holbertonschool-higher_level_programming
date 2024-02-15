61d9142d3ee1c73038ef2464fc4074fc11ad4119
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 <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>
2024-02-14 20:25:02 -0800

    16. JSON string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,15 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        lst = []
+        with open(cls.__name__ + ".json", 'w', encoding="utf-8") as f:
+            if list_objs is None:
+                pass
+            else:
+                for x in list_objs:
+                    lst.append(x.to_dictionary())
+            f.write(cls.to_json_string(lst))
