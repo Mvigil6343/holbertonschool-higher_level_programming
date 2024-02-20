3fa78d893976c44b03861566387f50306fa868eb
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 <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>
2024-02-20 04:41:32 -0800

    function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise  TypeError('a must be an integer')
+    if type(b) is not int:
+        raise  TypeError('b must be an integer')
+    return a + b
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
