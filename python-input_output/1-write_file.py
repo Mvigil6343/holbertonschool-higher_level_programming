281c2f6252a9a5de2aa101634fa0eef1a6c60bc6
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 <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>
2024-02-26 18:52:24 -0800

    update

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 def write_file(filename="", text=""):
     """function that writes on a file"""
+    
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
     return len(text)
